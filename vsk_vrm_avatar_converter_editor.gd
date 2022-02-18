cadbfe2d283c3fb15090e9b2f725e7b9d287c8ae
SaracenOne <SaracenOne@gmail.com>
2022-02-18 11:40:50 +0000

    Fix leaks and avatar conversion

--- vsk_vrm_avatar_converter_editor.gd
+++ vsk_vrm_avatar_converter_editor.gd
@@ -3,7 +3,6 @@ extends Node
 
 const vrm_toplevel_const = preload("res://addons/vrm/vrm_toplevel.gd")
 
-const vsk_avatar_definition_editor_const = preload("res://addons/vsk_avatar/vsk_avatar_definition_editor.gd")
 const vsk_vrm_avatar_functions_const = preload("vsk_vrm_avatar_functions.gd")
 const vsk_vrm_callback_const = preload("vsk_vrm_callback.gd")
 
@@ -13,9 +12,7 @@ enum {
 
 const VRM_EXTENSION = "vrm"
 
-var icon: Texture2D = null
 var node: Node = null
-var options: MenuButton = null
 var editor_plugin: EditorPlugin = null
 var err_dialog : AcceptDialog = null
 var save_dialog : FileDialog = null
@@ -31,7 +28,7 @@ func error_callback(p_err: int, p_extra_code: int) -> void:
 		err_dialog.set_text(error_str)
 		err_dialog.popup_centered_clamped()
 		
-func _menu_option(p_id : int) -> void:
+func menu_option(p_id : int) -> void:
 	match p_id:
 		MENU_OPTION_CONVERT_TO_VSK_AVATAR:
 			save_vrm_selection_dialog()
@@ -96,53 +93,43 @@ func save_vrm_selection_dialog() -> void:
 		save_dialog.current_file = str(node.name) + ".scn"
 		save_dialog.popup_centered_ratio()
 
-
-func _exit_tree():
-	if save_dialog and not save_dialog.is_queued_for_deletion():
-		save_dialog.queue_free()
-		
-	if err_dialog and not err_dialog.is_queued_for_deletion():
-		err_dialog.queue_free()
-		
-	if avatar_editor_root and not avatar_editor_root.is_queued_for_deletion():
-		avatar_editor_root.queue_free()
-		
-	if editor_plugin:
-		editor_plugin.remove_control_from_container(EditorPlugin.CONTAINER_SPATIAL_EDITOR_MENU, options)
-
 func edit(p_node: Node) -> void:
 	node = p_node
 
+func setup_dialogs() -> void:
+	err_dialog = AcceptDialog.new()
+	editor_plugin.get_editor_interface().get_base_control().add_child(err_dialog)
+	
+	save_dialog = FileDialog.new()
+	save_dialog.set_title("Save Avatar As...")
+	save_dialog.add_filter("*.scn; Scenes");
+	save_dialog.mode = FileDialog.FILE_MODE_SAVE_FILE
+	save_dialog.access = FileDialog.ACCESS_FILESYSTEM
+	save_dialog.connect("file_selected", Callable(self, "_save_file_at_path"))
+	editor_plugin.get_editor_interface().get_base_control().add_child(save_dialog)
+	
+func teardown_dialogs() -> void:
+	if err_dialog:
+		if err_dialog.is_inside_tree():
+			err_dialog.get_parent().remove_child(err_dialog)
+		err_dialog.queue_free()
+		
+	if save_dialog:
+		if save_dialog.is_inside_tree():
+			save_dialog.get_parent().remove_child(err_dialog)
+		save_dialog.queue_free()
+	
+func _enter_tree():
+	setup_dialogs()
+	
+	avatar_editor_root = Node3D.new()
+	avatar_editor_root.set_name("AvatarEditorRoot")
+	
+func _exit_tree():
+	teardown_dialogs()
+	
+	if avatar_editor_root:
+		avatar_editor_root.queue_free()
 
-func _ready():
-	if editor_plugin:
-		# Save
-		save_dialog = FileDialog.new()
-		save_dialog.set_title("Save Avatar As...")
-		save_dialog.add_filter("*.scn; Scenes");
-		save_dialog.mode = FileDialog.FILE_MODE_SAVE_FILE
-		save_dialog.access = FileDialog.ACCESS_FILESYSTEM
-		save_dialog.connect("file_selected", Callable(self, "_save_file_at_path"))
-		editor_plugin.get_editor_interface().get_viewport().add_child(save_dialog, true)
-		
-		err_dialog = AcceptDialog.new()
-		editor_plugin.get_editor_interface().get_viewport().add_child(err_dialog, true)
-		
-		# Spatial
-		avatar_editor_root = Node3D.new()
-		avatar_editor_root.set_name("AvatarEditorRoot")
-
-		options = MenuButton.new()
-		options.set_switch_on_hover(true)
-		
-		editor_plugin.add_control_to_container(EditorPlugin.CONTAINER_SPATIAL_EDITOR_MENU, options)
-		options.set_text("VRM")
-		options.set_button_icon(icon)
-		options.get_popup().add_item("Convert to VSK Avatar", MENU_OPTION_CONVERT_TO_VSK_AVATAR)
-		
-		options.get_popup().connect("id_pressed", Callable(self, "_menu_option"))
-		options.hide()
-
-func _init(p_editor_plugin: EditorPlugin, p_icon: Texture2D):
+func _init(p_editor_plugin: EditorPlugin):
 	editor_plugin = p_editor_plugin
-	icon = p_icon
